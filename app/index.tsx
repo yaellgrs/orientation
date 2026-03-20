ff52d9533628ae03eaff7ed24a0b031d76310716
yaellgrs <yaellegris09@gmail.com>
2026-03-21 00:05:25 +0100

    création de la map

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
+
+export default function Map() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
