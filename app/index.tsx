fee599a4197c8f4a2a488e7a6aebc90e0b82f91f
yaellgrs <yaellegris09@gmail.com>
2026-03-21 12:44:04 +0100

    map fonctionnelle

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,15 @@ import MapView from 'react-native-maps';
 export default function Map() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+  <MapView
+    style={styles.map}
+    initialRegion={{
+      latitude: 49.20455850994528,
+      longitude: -0.36739465028753276,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}//49.20455850994528, -0.36739465028753276
+  />
     </View>
   );
 }
